98d114dc98bb8c2a1b0f6e8f6dbf0e3abfc3b4ce
Wayne Thursby <wayne.thursby@1srq.com>
2019-12-13 19:38:50 -0500

    Updated to dynamically list YouTube videos.

--- .eleventy.js
+++ .eleventy.js
@@ -3,94 +3,131 @@ const CleanCSS = require("clean-css");
 const UglifyJS = require("uglify-es");
 const htmlmin = require("html-minifier");
 
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
+const fs = require('fs');
 
-  // Date formatting (human readable)
-  eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj).toFormat("dd LLL yyyy");
-  });
+console.log("Getting YT videos");
+//rss url for your videos
+const url = 'https://www.youtube.com/feeds/videos.xml?channel_id=UC1JNnoFiWxeTe-nyorgAZFw';
 
-  // Date formatting (machine readable)
-  eleventyConfig.addFilter("machineDate", dateObj => {
-    return DateTime.fromJSDate(dateObj).toFormat("yyyy-MM-dd");
-  });
-
-  // Minify CSS
-  eleventyConfig.addFilter("cssmin", function(code) {
-    return new CleanCSS({}).minify(code).styles;
-  });
-
-  // Minify JS
-  eleventyConfig.addFilter("jsmin", function(code) {
-    let minified = UglifyJS.minify(code);
-    if (minified.error) {
-      console.log("UglifyJS error: ", minified.error);
-      return code;
+let Parser = require('rss-parser');
+let parser = new Parser({
+    customFields: {
+      item: [
+        ['media:group', 'group'],
+      ]
     }
-    return minified.code;
   });
 
-  // Minify HTML output
-  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
-    if (outputPath.indexOf(".html") > -1) {
-      let minified = htmlmin.minify(content, {
-        useShortDoctype: true,
-        removeComments: true,
-        collapseWhitespace: true
-      });
-      return minified;
-    }
-    return content;
-  });
 
-  // only content in the `posts/` directory
-  eleventyConfig.addCollection("posts", function(collection) {
-    return collection.getAllSorted().filter(function(item) {
-      return item.inputPath.match(/^\.\/posts\//) !== null;
+let videos = [];
+
+(async() => {
+    let feed = await parser.parseURL(url);
+    feed.items.forEach(item => {
+        //create a videoId from id
+        item.videoId = item.id.split(":").pop();
+        item.description = item.group["media:description"];
+        // tmp = item.group["media:thumbnail"];
+        // holy garbage batman this was a major pain to figure out.
+        item.thumbnail = item.group["media:thumbnail"][0]["$"];
+        item.views = item.group["media:community"][0]["media:statistics"][0]["$"]["views"];
+        videos.push(item);
     });
-  });
 
-  // Don't process folders with static assets e.g. images
-  eleventyConfig.addPassthroughCopy("favicon.ico");
-  eleventyConfig.addPassthroughCopy("static/img");
-  eleventyConfig.addPassthroughCopy("admin");
-  eleventyConfig.addPassthroughCopy("_includes/assets/");
+    let JSONText = JSON.stringify(videos, null, 2);
 
-  /* Markdown Plugins */
-  let markdownIt = require("markdown-it");
-  let markdownItAnchor = require("markdown-it-anchor");
-  let options = {
-    html: true,
-    breaks: true,
-    linkify: true
-  };
-  let opts = {
-    permalink: false
-  };
+    fs.writeFileSync('./_data/videos.json', JSONText);
 
-  eleventyConfig.setLibrary("md", markdownIt(options)
-    .use(markdownItAnchor, opts)
-  );
+})();
 
-  return {
-    templateFormats: ["md", "njk", "html", "liquid"],
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
 
-    // If your site lives in a different subdirectory, change this.
-    // Leading or trailing slashes are all normalized away, so don’t worry about it.
-    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
-    // This is only used for URLs (it does not affect your file structure)
-    pathPrefix: "/",
+    // Date formatting (human readable)
+    eleventyConfig.addFilter("readableDate", dateObj => {
+        return DateTime.fromJSDate(dateObj).toFormat("dd LLL yyyy");
+    });
 
-    markdownTemplateEngine: "liquid",
-    htmlTemplateEngine: "njk",
-    dataTemplateEngine: "njk",
-    passthroughFileCopy: true,
-    dir: {
-      input: ".",
-      includes: "_includes",
-      data: "_data",
-      output: "_site"
-    }
-  };
+    // Date formatting (machine readable)
+    eleventyConfig.addFilter("machineDate", dateObj => {
+        return DateTime.fromJSDate(dateObj).toFormat("yyyy-MM-dd");
+    });
+
+    // Minify CSS
+    eleventyConfig.addFilter("cssmin", function(code) {
+        return new CleanCSS({}).minify(code).styles;
+    });
+
+    // Minify JS
+    eleventyConfig.addFilter("jsmin", function(code) {
+        let minified = UglifyJS.minify(code);
+        if (minified.error) {
+            console.log("UglifyJS error: ", minified.error);
+            return code;
+        }
+        return minified.code;
+    });
+
+    // Minify HTML output
+    eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+        if (outputPath.indexOf(".html") > -1) {
+            let minified = htmlmin.minify(content, {
+                useShortDoctype: true,
+                removeComments: true,
+                collapseWhitespace: true
+            });
+            return minified;
+        }
+        return content;
+    });
+
+    // only content in the `posts/` directory
+    eleventyConfig.addCollection("posts", function(collection) {
+        return collection.getAllSorted().filter(function(item) {
+            return item.inputPath.match(/^\.\/posts\//) !== null;
+        });
+    });
+
+    // Don't process folders with static assets e.g. images
+    eleventyConfig.addPassthroughCopy("favicon.ico");
+    eleventyConfig.addPassthroughCopy("static/img");
+    eleventyConfig.addPassthroughCopy("admin");
+    eleventyConfig.addPassthroughCopy("_includes/assets/");
+
+    /* Markdown Plugins */
+    let markdownIt = require("markdown-it");
+    let markdownItAnchor = require("markdown-it-anchor");
+    let options = {
+        html: true,
+        breaks: true,
+        linkify: true
+    };
+    let opts = {
+        permalink: false
+    };
+
+    eleventyConfig.setLibrary("md", markdownIt(options)
+        .use(markdownItAnchor, opts)
+    );
+
+    return {
+        templateFormats: ["md", "njk", "html", "liquid"],
+
+        // If your site lives in a different subdirectory, change this.
+        // Leading or trailing slashes are all normalized away, so don’t worry about it.
+        // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+        // This is only used for URLs (it does not affect your file structure)
+        pathPrefix: "/",
+
+        markdownTemplateEngine: "liquid",
+        htmlTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
+        passthroughFileCopy: true,
+        dir: {
+            input: ".",
+            includes: "_includes",
+            data: "_data",
+            output: "_site"
+        }
+    };
 };
--- pages/home.md
+++ pages/home.md
@@ -7,14 +7,12 @@ navtitle: Home
 tags:
   - nav
 ---
-# Welcome
+# Testing
 
-This is a template for building a simple blog website with the [Eleventy static site generator](https://www.11ty.io), with deployment to [Netlify](https://www.netlify.com).
+{% for vid in videos %}
 
-Includes [Netlify CMS](https://www.netlifycms.org) for WYSIWYG content editing, and [Netlify Forms](https://www.netlify.com/docs/form-handling) for processing your site's form data.
+<iframe width="560" height="315" src="https://www.youtube.com/embed/{{vid.videoId}}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> 
 
-For more info on installation and usage, view the [project repo]({{ pkg.repository.url }}) on Github.
+{{vid.description | escape }}
 
-Or click the button below to deploy your own copy of this project to Netlify.
-
-[![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/danurbanowicz/eleventy-netlify-boilerplate&stack=cms)
+{% endfor %}
